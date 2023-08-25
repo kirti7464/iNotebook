cba4d583749b21c36bd71f80390cb8d3d78eaa5b
kirti7464 <71482336+kirti7464@users.noreply.github.com>
2023-08-25 14:13:12 +0530

    Update NoteState.js

--- src/context/NoteState.js
+++ src/context/NoteState.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props)=>{
-    const host = "http://localhost:5000"
+    const host = "https://playful-tender-sprite.glitch.me/"
     const notesInitial=[]
     const [notes, setNotes] = useState(notesInitial)
       // Get all Notes
